b9b89d911be186915387b20aef2924abf2cf9c31
DarkhanNW <nwdarkhan@gmail.com>
2025-08-25 10:55:49 +0800

    update set_2.py

--- set_2.py
+++ set_2.py
@@ -1,27 +1,32 @@
 import string
 
+list_uppercase_letters = list(string.ascii_uppercase)
+
 def shift_letter(letter, shift):
-    list_of_letters = list(string.ascii_uppercase)
     if letter == " ":
         return " "
-    new_letter_index = list_of_letters.index(letter) + shift
+    new_letter_index = list_uppercase_letters.index(letter) + shift
     if new_letter_index >= 26:
         new_letter_index = new_letter_index % 26
-        return list_of_letters[new_letter_index]
-    return list_of_letters[new_letter_index]
+        return list_uppercase_letters[new_letter_index]
+    return list_uppercase_letters[new_letter_index]
 
 def caesar_cipher(message, shift):
-    pass
+    list_message = list(message)
+    list_new_message = []
+    for letter in list_message:
+        list_new_message.append(shift_letter(letter, shift))
+    new_message_joined = "".join(list_new_message)
+    return new_message_joined
 
 def shift_by_letter(letter, letter_shift):
-    list_of_letters = list(string.ascii_uppercase)
     if letter == " ":
         return " "
-    new_letter_index = list_of_letters.index(letter) + list_of_letters.index(letter_shift)
+    new_letter_index = list_uppercase_letters.index(letter) + list_uppercase_letters.index(letter_shift)
     if new_letter_index >= 26:
         new_letter_index = new_letter_index % 26
-        return list_of_letters[new_letter_index]
-    return list_of_letters[new_letter_index]
+        return list_uppercase_letters[new_letter_index]
+    return list_uppercase_letters[new_letter_index]
 
 def main():
     pass
