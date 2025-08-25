81c25261cf2c1edd836538aad2ca1fcbed3fa0bc
DarkhanNW <nwdarkhan@gmail.com>
2025-08-25 10:38:31 +0800

    update set_2.py

--- set_2.py
+++ set_2.py
@@ -10,6 +10,21 @@ def shift_letter(letter, shift):
         return list_of_letters[new_letter_index]
     return list_of_letters[new_letter_index]
 
-print(shift_letter(" ", 1))
-for len in range(0, 27):
-    print(shift_letter("B", len))
+def caesar_cipher(message, shift):
+    pass
+
+def shift_by_letter(letter, letter_shift):
+    list_of_letters = list(string.ascii_uppercase)
+    if letter == " ":
+        return " "
+    new_letter_index = list_of_letters.index(letter) + list_of_letters.index(letter_shift)
+    if new_letter_index >= 26:
+        new_letter_index = new_letter_index % 26
+        return list_of_letters[new_letter_index]
+    return list_of_letters[new_letter_index]
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
