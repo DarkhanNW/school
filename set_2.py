42fef0d46313b9ac8f646cd32beb420f0c10ca41
DarkhanNW <nwdarkhan@gmail.com>
2025-08-25 15:56:01 +0800

    update set_2.py

--- set_2.py
+++ set_2.py
@@ -61,10 +61,24 @@ def scytale_cipher(message, shift):
 
     new_message_joined = "".join(list_new_message)
     return new_message_joined
+
+def scytale_decipher(message, shift):
+    list_message = list(message)
+    new_message = ""
+    row_num = 0
+    while len(list_message) % shift != 0:
+        list_message.append("_")
     
-    
+    for i in range(0, shift):
+        new_message_part = (list_message[row_num::shift])
+        new_message += "".join(new_message_part)
+        row_num += 1
+
+    return new_message
+
 def main():
     print(scytale_cipher("INFORMATION_AGE", 3))
+    print(scytale_decipher("IMNNA_FTAOIGROE", 3))
 
 if __name__ == "__main__":
     main()
