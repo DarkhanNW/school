95fd0b5f26ad8475e34dc0c5c4f0c4c6a353060b
DarkhanNW <nwdarkhan@gmail.com>
2025-08-27 17:53:31 +0800

    update set_3.py

--- set_3.py
+++ set_3.py
@@ -79,6 +79,18 @@ board7 = [
 ['O','X','','']
 ]
 
+legs = {
+     ("upd","admu"):{
+         "travel_time_mins":10
+     },
+     ("admu","dlsu"):{
+         "travel_time_mins":35
+     },
+     ("dlsu","upd"):{
+         "travel_time_mins":55
+     }
+}
+
 def relationship_status(from_member, to_member, social_graph):
     if (from_member in social_graph[to_member]["following"]) and (to_member in social_graph[from_member]["following"]) == True:
         return "friends"
@@ -111,10 +123,44 @@ def tic_tac_toe(board):
     
     return "NO WINNER"
 
+def eta(first_stop, second_stop, route_map):
+    travel_time = 0
+    trip_processing = True
+    trip_started = False
+    trip_start = None
+    trip_end = None
 
+    for route in route_map:
+        if first_stop in route[0]:
+            trip_start = route
+        if second_stop in route[1]:
+            trip_end = route
+    
+    while trip_processing:
+        for route in route_map:
+            if route == trip_start:
+                if route == trip_end:
+                    travel_time += route_map[route]["travel_time_mins"]
+                    trip_processing = False
+                    break
+                travel_time += route_map[trip_start]["travel_time_mins"]
+                trip_started = True
+                continue
+            if trip_started:
+                if route == trip_end:
+                    trip_processing = False
+                    travel_time += route_map[route]["travel_time_mins"]
+                    break
+                travel_time += route_map[route]["travel_time_mins"]
 
+    return travel_time
+
+# for my own testing
 def main():
-    print(tic_tac_toe(board1))
+    print(eta("admu", "dlsu", legs))
+    print(eta("upd", "upd", legs))
+    print(eta("admu", "upd", legs))
+    print(eta("upd", "dlsu", legs))
 
 if __name__ == "__main__":
     main()
